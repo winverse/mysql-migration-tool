7fb860d1f82d37d3f85bca2e35323ccfe916489b
sangHyun <public.winverse@gmail.com>
2022-10-07 03:21:40 +0900

    Docs: Update README.md

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
-# 개발 환경
+# Motivation
+- [Sequelize](https://sequelize.org/)에 존재하는 migration 기능을 보고서 한번 구현해보고 싶었습니다.
+- 간단하게 만들기 위해서 외부 의존성은 mysql2만 사용하였습니다.
 
-### mysql
+# Simple mysql migration tool
+- 데이터 베이스 스키마를 기록하여 다른 데이터 베이스에 migration을 자동으로 수행해주는 프로그램입니다.
+- `yarn generate` 명령어를 통해서 데이터 베이스에 대한 변화를 추가적으로 감지하여 변경 된 부분만 데이터 베이스 스키마를 기록합니다.
+- `yarn run <DBName>` migration을 적용할 DB 이름을 적어주면 변경된 사항들을 단계적으로 적용하고 적용된 사항들을 DB에 기록합니다.
+- 테이블에 대한 기본적인 DDL 문법들이 적용 가능합니다.
 
-- Ver 8.0.23 for osx10.16 on x86_64 (Homebrew)
+# API
+- [X] createTable
+- [X] dropTable
+- [X] addColumn
+- [X] removeColumn
+- [X] changeColumn
 
-### nodejs
-
-- v16.13.0
-
-### yarn
-
-- 1.22.17
-
-# 테스트
+# 실행
 
 - config 경로에 config.example.json 을 복사한뒤에 config.json 파일을 만들고 내용을 환경에 맞게 적어주세요
 
@@ -24,4 +27,4 @@
 - `yarn run <DBName>`: migration 파일에 저장된 사항들을 다른 DB에 적용합니다.   
   만약 DBName을 입력하지 않는다면 에러가 발생합니다.
 
-- `yarn reset`: 삭제해도 무관한 파일들을 삭제합니다.
+- `yarn reset`: 삭제해도 괜찮은 무관한 파일들을 삭제합니다.
--- database/migration-runner.ts
+++ database/migration-runner.ts
@@ -1,45 +1,48 @@
-import fs from 'fs';
-import getMigrationDir from '../lib/migration-dir';
-import { DiffData } from '../types';
-import query from './query/query';
+import fs from "fs";
+import getMigrationDir from "../lib/migration-dir";
+import { DiffData } from "../types";
+import query from "./query/query";
+
 class MigrationRunner {
-  private databaseName: string = '';
-  private metaTableName: string = '';
-  private isDev = process.env.NODE_ENV !== 'production';
+  private databaseName: string = "";
+  private metaTableName: string = "";
+  private isDev = process.env.NODE_ENV !== "production";
 
   private constructor(databaseName: string, metaTableName: string) {
     this.databaseName = databaseName;
     this.metaTableName = metaTableName;
   }
 
-  static setDatabaseName (databaseName: string, metaTableName: string) {
+  static setDatabaseName(databaseName: string, metaTableName: string) {
     return new MigrationRunner(databaseName, metaTableName);
   }
 
-  private async migrationRecord (filenames: string[]): Promise<number> {
+  private async migrationRecord(filenames: string[]): Promise<number> {
     const appliedFileCount = filenames.length;
     if (appliedFileCount) {
       try {
         const uniqueFilenames = Array.from(new Set(filenames));
-        await Promise.all(uniqueFilenames.map(async(filename) => {
-          const rawQuery = `INSERT INTO \`${this.databaseName}\`.\`${this.metaTableName}\` 
+        await Promise.all(
+          uniqueFilenames.map(async (filename) => {
+            const rawQuery = `INSERT INTO \`${this.databaseName}\`.\`${this.metaTableName}\` 
             (name) VALUE ('${filename}');
           `;
-          await query(rawQuery);
-          console.log(`filename: '${filename}' was recored`);
-        }));
+            await query(rawQuery);
+            console.log(`filename: '${filename}' was recored`);
+          })
+        );
       } catch (err) {
         throw new Error(err as string);
       }
     } else {
-      console.error('[FAIL] All migration has already been applied');
+      console.error("[FAIL] All migration has already been applied");
     }
     return appliedFileCount;
   }
 
   async migrationRunner(): Promise<number> {
     const migrationDir = getMigrationDir();
-    let queries: { rawQuery: string, filename: string }[] = [];
+    let queries: { rawQuery: string; filename: string }[] = [];
 
     fs.readdirSync(migrationDir).map((file) => {
       let up;
@@ -48,23 +51,29 @@ class MigrationRunner {
       } else {
         up = require(`./migration/${file}`);
       }
-      up.map((diffContext: DiffData) => queries.push({ rawQuery: diffContext.query, filename: file }));
-    })
+      up.map((diffContext: DiffData) =>
+        queries.push({ rawQuery: diffContext.query, filename: file })
+      );
+    });
 
-    const filenames = await Promise.all(queries.map(async({ rawQuery, filename }) => {
-      try {
-        const alreadyRunMigation = await query(`SELECT name FROM \`${this.databaseName}\`.\`${this.metaTableName}\` where name = '${filename}' LIMIT 1;`);
-        if (alreadyRunMigation.length <= 0) {
-          await query(rawQuery.replace('DATABASE-NAME', this.databaseName));
-          return filename;
+    const filenames = await Promise.all(
+      queries.map(async ({ rawQuery, filename }) => {
+        try {
+          const alreadyRunMigation = await query(
+            `SELECT name FROM \`${this.databaseName}\`.\`${this.metaTableName}\` where name = '${filename}' LIMIT 1;`
+          );
+          if (alreadyRunMigation.length <= 0) {
+            await query(rawQuery.replace("DATABASE-NAME", this.databaseName));
+            return filename;
+          }
+        } catch (err) {
+          throw new Error(err as string);
         }
-      } catch (err) {
-        throw new Error(err as string);
-      }
-    }))
+      })
+    );
 
     // migration 기록
-    const clean = filenames.filter(Boolean); 
+    const clean = filenames.filter(Boolean);
     const appliedFileCount = await this.migrationRecord(clean as string[]);
     return appliedFileCount;
   }
